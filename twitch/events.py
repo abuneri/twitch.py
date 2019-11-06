d30252a81a4242705293980d58c78c6897a3ac05
Ashton <ashtonater@gmail.com>
2019-11-05 18:34:18 -0800

    theres no advantage of having Event be a named tuple, just change to regular class with static constants

--- twitch/events.py
+++ twitch/events.py
@@ -47,52 +47,29 @@ AUTHENTICATED = '_authenticated'
 
 """
 
-EventDef = namedtuple('EventDef', 'PINGED '
-                                  'PONGED '
-                                  'CONNECTED '
-                                  'DISCONNECT '
-                                  'SOCKET_SEND '
-                                  'SOCKET_RECEIVE '
-                                  'MESSAGE '
-                                  'USER_JOIN_CHANNEL '
-                                  'USER_LEFT_CHANNEL '
-                                  'LIST_USERS '
-                                  'MOD_STATUS_CHANGED '
-                                  'CHAT_CLEARED '
-                                  'MESSAGE_CLEARED '
-                                  'HOST_MODE_CHANGED '
-                                  'CHANNELS_REJOINED '
-                                  'CHANNEL_STATE_CHANGED '
-                                  'USER_NOTIFICATION '
-                                  'USER_STATE_CHANGED '
-                                  'UNKNOWN '
-                                  'TAG_REQUEST_ACKED '
-                                  'MEMBERSHIP_REQUEST_ACKED '
-                                  'COMMANDS_REQUEST_ACKED '
-                                  'CHAT_ROOMS_REQUEST_ACKED '
-                                  'AUTHENTICATED')
 
-Event = EventDef(PINGED='ping',
-                 PONGED='pong',
-                 CONNECTED='connected',
-                 DISCONNECT='disconnect',
-                 SOCKET_SEND='socket_send',
-                 SOCKET_RECEIVE='socket_receive',
-                 MESSAGE='message',
-                 USER_JOIN_CHANNEL='user_join_channel',
-                 USER_LEFT_CHANNEL='user_leave_channel',
-                 LIST_USERS='list_users',
-                 MOD_STATUS_CHANGED='mod_status_updated',
-                 CHAT_CLEARED='chat_cleared',
-                 MESSAGE_CLEARED='message_cleared',
-                 HOST_MODE_CHANGED='host_mode_updated',
-                 CHANNELS_REJOINED='channels_rejoined',
-                 CHANNEL_STATE_CHANGED='channel_state_update',
-                 USER_NOTIFICATION='user_noitification',
-                 USER_STATE_CHANGED='user_updated',
-                 TAG_REQUEST_ACKED='tag_request_acked',
-                 MEMBERSHIP_REQUEST_ACKED='membership_request_acked',
-                 COMMANDS_REQUEST_ACKED='commands_request_acked',
-                 CHAT_ROOMS_REQUEST_ACKED='chat_rooms_request_acked',
-                 UNKNOWN='unknown',
-                 AUTHENTICATED='_authenticated')
+class Event:
+    PINGED = 'ping'
+    PONGED = 'pong'
+    CONNECTED = 'connected'
+    DISCONNECT = 'disconnect'
+    SOCKET_SEND = 'socket_send'
+    SOCKET_RECEIVE = 'socket_receive'
+    MESSAGE = 'message'
+    USER_JOIN_CHANNEL = 'user_join_channel'
+    USER_LEFT_CHANNEL = 'user_leave_channel'
+    LIST_USERS = 'list_users'
+    MOD_STATUS_CHANGED = 'mod_status_updated'
+    CHAT_CLEARED = 'chat_cleared'
+    MESSAGE_CLEARED = 'message_cleared'
+    HOST_MODE_CHANGED = 'host_mode_updated'
+    CHANNELS_REJOINED = 'channels_rejoined'
+    CHANNEL_STATE_CHANGED = 'channel_state_update'
+    USER_NOTIFICATION = 'user_noitification'
+    USER_STATE_CHANGED = 'user_updated'
+    TAG_REQUEST_ACKED = 'tag_request_acked'
+    MEMBERSHIP_REQUEST_ACKED = 'membership_request_acked'
+    COMMANDS_REQUEST_ACKED = 'commands_request_acked'
+    CHAT_ROOMS_REQUEST_ACKED = 'chat_rooms_request_acked'
+    UNKNOWN = 'unknown'
+    AUTHENTICATED = '_authenticated'
